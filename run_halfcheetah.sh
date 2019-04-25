4edff2999a378f9b8825283940a0811b936ff11a
fiberleif <fiberleif@gmail.com>
2019-04-25 11:31:28 +0800

    4/25/2019: Change sh config

--- run_halfcheetah.sh
+++ run_halfcheetah.sh
@@ -3,5 +3,5 @@
 SEEDS="0 1 2 3 4"
 for SEED in $SEEDS
 do
-	python baselines/gail/run_mujoco.py --env_id HalfCheetah-v1 --seed $SEED --num_epochs 2000 --expert_path dataset/half_cheetah.npz
+	python baselines/gail/run_mujoco.py --env_id HalfCheetah-v1 --seed $SEED --num_epochs 1000 --expert_path dataset/half_cheetah.npz
 done
