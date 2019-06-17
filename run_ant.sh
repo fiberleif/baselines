bfebffc5ef654b032faf7add296200befebef78d
fiberleif <fiberleif@gmail.com>
2019-06-17 15:26:29 +0800

    Update the shells for ant and humanoid.

--- run_ant.sh
+++ run_ant.sh
@@ -3,5 +3,5 @@
 SEEDS="0 1 2 3 4"
 for SEED in $SEEDS
 do
-	python baselines/gail/run_mujoco.py --env_id Ant-v1 --seed $SEED --num_epochs 400 --timesteps_per_batch 5000 --eval_interval 4 --expert_path dataset/ant.npz
+	python baselines/gail/run_mujoco.py --env_id Ant-v1 --seed $SEED --num_epochs 400 --timesteps_per_batch 5000 --evaluation_freq 4 --expert_path dataset/ant.npz
 done
