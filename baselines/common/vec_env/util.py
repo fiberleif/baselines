894529f7e1ac49e5c7edd7dc478c904a2d8c1518
fiberleif <fiberleif@gmail.com>
2019-06-18 22:07:09 +0800

    Add evaluation part for trpo.

--- baselines/common/vec_env/util.py
+++ baselines/common/vec_env/util.py
@@ -35,11 +35,13 @@ def obs_space_info(obs_space):
         shapes: a dict mapping keys to shapes.
         dtypes: a dict mapping keys to dtypes.
     """
-    if isinstance(obs_space, gym.spaces.Dict):
-        assert isinstance(obs_space.spaces, OrderedDict)
-        subspaces = obs_space.spaces
-    else:
-        subspaces = {None: obs_space}
+    # if isinstance(obs_space, gym.spaces.Dict):
+    #     assert isinstance(obs_space.spaces, OrderedDict)
+    #     subspaces = obs_space.spaces
+    # else:
+    #     subspaces = {None: obs_space}
+    subspaces = {None: obs_space}
+
     keys = []
     shapes = {}
     dtypes = {}
