919f3710edcca763b261de01988f13d67e263d53
fiberleif <fiberleif@gmail.com>
2019-06-27 12:59:24 +0800

    Add pretrain for TRPO.

--- baselines/trpo_mpi/behavior_clone.py
+++ baselines/trpo_mpi/behavior_clone.py
@@ -49,7 +49,7 @@ def learn(ob, policy_func, dataset, optim_batch_size=128, max_iters=1e4,
     # ob = U.get_placeholder_cached(name="ob")
     ac = pi.pdtype.sample_placeholder([None])
     # stochastic = U.get_placeholder_cached(name="stochastic")
-    loss = tf.reduce_mean(tf.square(ac-pi.ac))
+    loss = tf.reduce_mean(tf.square(ac-pi.pi))
     var_list = pi.get_trainable_variables()
     adam = MpiAdam(var_list, epsilon=adam_epsilon)
     # lossandgrad = U.function([ob, ac, stochastic], [loss]+[U.flatgrad(loss, var_list)])
